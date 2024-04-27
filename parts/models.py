63cd22ccb72bc3a2bd9a5527b3eeaa75d4d8ca5a
Aiden <aiden.rinas@gmail.com>
2024-04-26 23:28:06 -0500

    Added models for the brand, CPU, GPU, storage, and RAM.

--- parts/models.py
+++ parts/models.py
@@ -1,3 +1,118 @@
 from django.db import models
 
 # Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(
+        max_length = 200,
+        unique = True,
+        help_text = "Enter the name of the brand"
+    )
+
+class CPU(models.Model):
+    name = models.CharField(
+        max_length = 200,
+        unique = True,
+        help_text = "Enter the name of the CPU"
+    )
+    brand = models.ManyToManyField(
+        Brand,
+        help_text = "Choose the company that made this CPU"
+    )
+    price = models.FloatField(
+        max_digits = 7,
+        decimal_places = 2
+    )
+    rank = models.IntegerField(
+        help_text = "Enter the rank of the CPU compared to other CPUs"
+    )
+    num_cores = models.IntegerField(
+        help_text = "Enter the number of cores the CPU has"
+    )
+    core_speed = models.IntegerField(
+        help_text = "Enter the speed of the cores in megahertz"
+    )
+
+class GPU(models.Model):
+    name = models.CharField(
+        max_length = 200,
+        unique = True,
+        help_text = "Enter the name of the GPU"
+    )
+    brand = models.ManyToManyField(
+        Brand,
+        help_text = "Choose the company that made this GPU"
+    )
+    price = models.FloatField(
+        max_digits = 7,
+        decimal_places = 2
+    )
+    rank = models.IntegerField(
+        help_text = "Enter the rank of the GPU compared to other GPUs"
+    )
+    memory = models.IntegerField(
+        help_text = "Enter the internal memory the GPU has"
+    )
+    core_speed = models.IntegerField(
+        help_text = "Enter the speed of the cores in megahertz"
+    )
+
+class Storage(models.Model):
+    name = models.CharField(
+        max_length = 200,
+        unique = True,
+        help_text = "Enter the name of the storage device"
+    )
+    brand = models.ManyToManyField(
+        Brand,
+        help_text = "Choose the company that made this storage device"
+    )
+    price = models.FloatField(
+        max_digits = 7,
+        decimal_places = 2
+    )
+    rank = models.IntegerField(
+        help_text = "Enter the rank of the storage device compared to other storage devices"
+    )
+    amount_of_space = models.IntegerField(
+        help_text = "Enter the amount of space in gigabytes"
+    )
+    TYPE_CHOICES = (
+        ("1", "HDD"),
+        ("2", "SSD")
+    )
+    type = models.CharField(
+        choices = TYPE_CHOICES,
+        help_text = ""
+    )
+
+class RAM(models.Model):
+    name = models.CharField(
+        max_length = 200,
+        unique = True,
+        help_text = "Enter the name of the RAM"
+    )
+    brand = models.ManyToManyField(
+        Brand,
+        help_text = "Choose the company that made this RAM"
+    )
+    price = models.FloatField(
+        max_digits = 7,
+        decimal_places = 2
+    )
+    rank = models.IntegerField(
+        help_text = "Enter the rank of the RAM compared to other RAM sticks"
+    )
+    TYPE_CHOICES = (
+        ("1", "DDR1"),
+        ("2", "DDR2"),
+        ("3", "DDR3"),
+        ("4", "DDR4")
+    )
+    DDR_type = models.CharField(
+        choices = TYPE_CHOICES,
+        help_text = "Choose the type of DDR this RAM is"
+    )
+    amount = models.IntegerField(
+        help_text = "Enter the amount of RAM one stick gives in gigabytes"
+    )
