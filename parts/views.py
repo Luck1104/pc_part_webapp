489ca3c2377ef63e357c55048969b8162b8599b6
Isaac Huntington <isaac.huntington@ndsu.edu>
2024-05-03 10:27:10 -0500

    updated db for making list views and start parts list view

--- parts/views.py
+++ parts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import *
 
 # Create your views here.
 
@@ -7,3 +9,6 @@ def index(request):
 
     }
     return render(request, 'index.html', context=context)
+
+class CPUListView(generic.ListView):
+    model = CPU
